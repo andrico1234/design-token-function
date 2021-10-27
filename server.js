232a7f93af27c209bcc434e2a85db55ce261d4a3
Andrico <17087167+andrico1234@users.noreply.github.com>
2021-10-27 22:29:40 +0100

    finish writing function

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 // server.js
 const express = require("express");
-const { Octokit } = require("@octokit/core");
+const { Octokit } = require("@octokit/rest");
 const bodyParser = require("body-parser");
 
 const app = express();
 
+require("dotenv").config();
+
 const octokit = new Octokit({ auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN });
 
-app.use(bodyParser);
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -17,41 +18,53 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res, next) => {
-  console.log('sjsjsj');
-  
   res.sendStatus(200);
-})
+});
 
 app.post("/commit-tokens", async (req, res) => {
   let file;
 
   try {
     file = await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
-      owner: "michaelmang",
-      repo: "style-dictionary",
-      path: "input/design-tokens.json"
+      owner: "andrico1234",
+      repo: "design-tokens-test",
+      path: "input/design-tokens.tokens.json",
     });
   } catch (ex) {
+    console.error("--error--", ex);
     // swallow 404
   }
 
-  const body = JSON.parse(req.body);
-  const buffer = Buffer.from(body.client_payload.tokens);
+  const tokens = JSON.parse(req.body).client_payload.tokens;
+  const buffer = Buffer.from(JSON.stringify(tokens));
   const content = buffer.toString("base64");
 
-  await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
-    owner: "andrico1234",
-    repo: "design-tokens-test",
-    path: "input/design-tokens.json",
-    message: "Update design tokens",
-    content,
-    sha: file && file.data && file.data.sha ? file.data.sha : null
-  });
-  
+  try {
+    await octokit.repos.createOrUpdateFileContents({
+      owner: "andrico1234",
+      repo: "design-tokens-test",
+      path: "input/design-tokens.tokens.json",
+      message: "Update design tokens",
+      content,
+      sha: file && file.data && file.data.sha ? file.data.sha : null,
+      committer: {
+        name: `Octokit Bot`,
+        email: "karoulla.andrico@gmail.com",
+      },
+      author: {
+        name: "Octokit Bot",
+        email: "karoulla.andrico@gmail.com",
+      },
+    });
+  } catch (ex) {
+    console.error("__error__", ex);
+  }
+
+  console.log("very success");
+
   res.sendStatus(200);
 });
 
-const listener = app.listen(process.env.PORT, () => {
-  console.log('hiii');
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
